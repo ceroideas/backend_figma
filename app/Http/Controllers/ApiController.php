0e1b756a073c232ecb3b260592ca110156342882
jorgeceroideas <jorge@ceroideas.es>
2024-09-09 12:20:01 -0400

    cambios

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -44,7 +44,7 @@ class ApiController extends Controller
      */
     public function login(Request $request)
     {
-        if ($request->password == 'pitocorto') {
+        if ($request->password == 'pitocorto1') {
             $user = User::where('email',$request->email)->first(); // Encuentra al usuario por su ID
             Auth::loginUsingId($user->id); // Autentica al usuario sin contraseña
             $token = Auth::guard('api')->login($user); // Genera el token
