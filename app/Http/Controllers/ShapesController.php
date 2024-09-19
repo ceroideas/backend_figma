960ab86e71bd28c698955ed5de71e62677648192
jorgeceroideas <jorge@ceroideas.es>
2024-09-20 00:34:23 +0200

    xambios

--- app/Http/Controllers/ShapesController.php
+++ app/Http/Controllers/ShapesController.php
@@ -407,6 +407,7 @@ class ShapesController extends Controller
 
                     $node = $this->projectNodes->firstWhere('id', $nodeId);
 
+                    if (!isset($this->csvData[$j])) {$this->csvData[$j] = ['id' => $this->simulationId];}
                     $this->csvData[$j] = array_merge($this->csvData[$j], ['id' => $this->simulationId]);
                     if ($node->type == 1) {
                         if (!in_array($node->id, $this->nodesActive)) {
